2d4f875fc9827f7e5df750f0ca48c7e49d76e79b
cristian martinez <criizt.mart@gmail.com>
2024-05-21 12:56:45 -0500

    hashing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HttpTokenInterceptor } from './shared/interceptor/token.interceptor'; 
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
  
 export const appConfig: ApplicationConfig = { 
    
   providers: [    
     provideRouter(routes),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     { provide: HTTP_INTERCEPTORS, useClass: HttpTokenInterceptor, multi: true },
   ]
 };
